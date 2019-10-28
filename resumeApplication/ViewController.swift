bc02caae99f90f1bdee2d94f5c82e06d6ab9992c
JosefinLindvall <lijo18zu@student.ju.se>
2019-10-28 15:53:50 +0100

    Initial Commit

---
+++ resumeApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  resumeApplication
+//
+//  Created by Josefin Lindvall on 2019-10-28.
+//  Copyright © 2019 JosefinLindvall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
